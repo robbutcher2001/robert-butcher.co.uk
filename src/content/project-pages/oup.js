a9ca4d58aa97f855ba6c45aea3725f99594db272
Rob Butcher <rob.butcher2@baesystems.com>
2018-04-18 14:23:07 +0100

    Code format

--- src/content/project-pages/oup.js
+++ src/content/project-pages/oup.js
@@ -6,7 +6,7 @@ const oupContent = {
   dates: 'April 2017 - April 2018',
   proseA: 'first paragraph',
   proseB: 'another paragraph',
-  code: '&lt;VideoPlayer{\'\n\'}  ref="videoplayer"{\'\n\'}  preload={\'{\'}this.props.preload{\'}\n\'}  classes={\'{\'}this.props.classes{\'}\n\'}  videoID={\'{\'}this.props.videoID{\'}\n\'}  controls="controls"{\'\n\'}/&gt;'
+  code: '<VideoPlayer\n  ref="videoplayer"\n  preload={this.props.preload}\n  classes={this.props.classes}\n  videoID={this.props.videoID}\n  controls="controls"\n/>'
 };
 
 module.exports = {
