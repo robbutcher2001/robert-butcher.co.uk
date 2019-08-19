c7f3d328171dbfcc41e68d95ee0b21faddb3293c
Rob Butcher <robbutcher2001@gmail.com>
2019-08-19 22:01:22 +0100

    Music content

---
+++ src/content/projects/full-pages/mixes.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'Rob\'s Recorded Mixes',
+  logoUrl: '/content/images/mixer.jpg',
+  logoLink: '/projects/mixes',
+  proseA: [
+    'As you may have already gathered, Rob has always loved his music. This love extended into manipulating music and eventually mixing it together during his first year of University when he started to experiment with mixing software.',
+    'Initially, mixing on software using a keyboard and mouse with VirtualDJ was fun but ultimately not practical. He decided to buy a MIDI controller to help control the software in real-time whilst mixing tracks together, eventually buying an external sound card and Numark mixer for real hands-off laptop experience. This is where he fell in love.',
+    'A few years at Uni learning to mix, he finally got a slot in Bar Med, Guildford and played this set:'
+  ],
+  mixA: 'Bar Med mix here',
+  proseB: [
+    '',
+    'I suppose the pinnacle of his fun in this industry is playing back at his hometown club in Dartford during the third year of his degree.',
+    'He\'d frequented this club a fair few times between 18 and 21 so to be behind the decks and playing a set to the crowd he used to be in was something quite special. Drum and bass was big at the time so even though it\'s not his music of choice, he had so much fun playing the following set to a crowd of a few hundred:'
+  ],
+  mixB: 'https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Frobbutcher%2Fcommercial-zen-mix%2F',
+  proseC: [
+    '',
+    'Rob now has a dedicated room in his flat with a Pioneer controller and KRK monitor set up and records mixes of varying genres as and when he can. Here are a few more he\'s recorded if you feel like a listen!'
+  ],
+  mixC: 'Trance mix here',
+};
--- src/content/projects/synopses/music-app.js
+++ src/content/projects/synopses/music-app.js
@@ -2,9 +2,9 @@ const musicAppContent = {
   title: 'Music App',
   prose: 'Some stuff about working at my music app',
   imageUrl: '/content/images/music-app.png',
-  imageLink: '/projects/tunage',
+  imageLink: '/projects/music-app',
   buttonText: 'Learn more (this can link to same page homepage does)',
-  buttonLink: '/projects/tunage'
+  buttonLink: '/projects/music-app'
 };
 
 module.exports = {
